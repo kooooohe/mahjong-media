14417121692055ae45257088e9b8348ddb5a6b75
kohekondo <1211lta@gmail.com>
2017-11-25 18:19:56 +0900

    環境構築し直して動くようにした

---
+++ playindoor/resources/assets/ts/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { ClarityModule } from 'clarity-angular';
+import { AppComponent } from './app.component';
+@NgModule({
+    imports: [
+        BrowserModule,
+        ClarityModule.forRoot()
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
